c8908dcd14c017c2621d2dceed16eb6a1443bf28
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-12-20 16:35:32 +0300

    Encoding state(Categorical(Nominal))Variable

--- Data Processing/data_proccessing_template.py
+++ Data Processing/data_proccessing_template.py
@@ -16,27 +16,11 @@ X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 # Encoding categorical data
 
+# Encoding categorical data
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder,LabelEncoder
-labelencoder_X = LabelEncoder()
-X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+from sklearn.preprocessing import OneHotEncoder
 
-columnTransformer = ColumnTransformer(
-    transformers=[
-        ('encoder', OneHotEncoder(), [0])
-    ],
-    remainder='passthrough'  
-)
-X= columnTransformer.fit_transform(X)
-labelencoder_Y=LabelEncoder()
-Y=labelencoder_Y.fit_transform(Y)
-#Spliting the Datset into Training set and test set
-from sklearn.model_selection import train_test_split
-
-# Assuming X and Y are your feature and target variables
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2,random_state=0)
-#Feature Scaling
-from sklearn.preprocessing import StandardScaler 
-sc_X=StandardScaler()
-X_train=sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
+# Assuming X is your dataset with the categorical column at index 0
+# ColumnTransformer for encoding categorical features
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+X = ct.fit_transform(X)
