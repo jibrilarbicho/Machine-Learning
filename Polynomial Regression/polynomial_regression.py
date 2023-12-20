acb1d5587029a07aa0b0d62c4224023f2444809d
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-12-20 22:38:55 +0300

    Visualizing polynomial Regression

--- Polynomial Regression/polynomial_regression.py
+++ Polynomial Regression/polynomial_regression.py
@@ -16,3 +16,23 @@ lin_reg = LinearRegression()
 lin_reg.fit(X, y)
 
 # Training the Polynomial Regression model on the whole dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg=PolynomialFeatures(degree=4)
+X_poly=poly_reg.fit_transform(X)
+lin_reg2=LinearRegression()
+lin_reg2.fit(X_poly,y)
+
+# Visualising the Linear Regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+# Visualising the Polynomial Regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg2.predict(X_poly), color = 'blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
