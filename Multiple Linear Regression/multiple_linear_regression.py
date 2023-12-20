ced9086e311d1d2448be7411739d02b730a3e0e2
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-12-20 17:00:44 +0300

    Predicting profit in Multiple Linear Regression

--- Multiple Linear Regression/multiple_linear_regression.py
+++ Multiple Linear Regression/multiple_linear_regression.py
@@ -23,3 +23,7 @@ from sklearn.model_selection import train_test_split
 
 # Assuming X and Y are your feature and target variables
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=0)
+from sklearn.linear_model import LinearRegression
+regressor=LinearRegression()
+regressor.fit(X_train,y_train)
+y_predict=regressor.predict(X_test)
