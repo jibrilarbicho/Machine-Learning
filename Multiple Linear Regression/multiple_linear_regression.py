d19255c58ad4685d34233f6e4b335f5a3c1f83bd
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-12-20 16:47:17 +0300

    Dataset spliting with test_size=0.2

--- Multiple Linear Regression/multiple_linear_regression.py
+++ Multiple Linear Regression/multiple_linear_regression.py
@@ -16,5 +16,10 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
 X = ct.fit_transform(X)
-
+#Avoiding  The Dummy Variable trap
+#X=X[:,1:] to make not select 1st column but we don't make this manually b/c there library that handle this
 print(X)
+from sklearn.model_selection import train_test_split
+
+# Assuming X and Y are your feature and target variables
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=0)
