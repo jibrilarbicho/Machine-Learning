dd54fde2a3335a14280a5d1c9ddc0951a76ffa8a
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-12-22 14:47:37 +0300

    support Vector Machine

--- 08-Logistic Regression/logistic_regression.py
+++ 08-Logistic Regression/logistic_regression.py
@@ -29,3 +29,27 @@ classifier.fit(X_train,y_train)
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print(accuracy_score(y_test, y_pred))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
